b62bc73eea8af87d47552bce6cd9e9db36b328ed
郑宁 <zn17326432274@63.com>
2021-01-11 10:52:55 +0800

    first commit

---
+++ js/防抖节流.js
@@ -0,0 +1,64 @@
+/**
+ *  onChange = {debunce(this.changeHandle,[delay])}  
+ */
+/**
+ * @name debounce 防抖
+ * @description 如果有定时器，则清除，以此往复，清除除了最后一个定时器之前所有的定时器，保证执行一次
+ * @param {*} fn 传入的函数 
+ * @param {*} delay 需要延时的时间
+ */
+function debounce(fn,delay) {
+  let timer = null
+  return function (arg) {
+    if (timer!= null) clearTimeout(timer)
+    timer = setTimeout(() => {
+      console.log(arg);
+      let value = ''
+      switch (arg.type) {
+        case 'change':
+          value = arg.target.value
+          break;
+        case 'input':
+          value = arg.target.value
+          break;
+        default:
+          break;
+      }
+      fn(value)
+    }, delay);
+  }
+}
+
+/**
+ * @name throttle 节流
+ * @description 判断时间变化是否在传入时间之外，是则执行，不是则不执行
+ * @param {*} fn 传入的函数 
+ * @param {*} delay 节流时间
+ */
+function throttle(fn,delay) {
+  let oldDate = new Date().getTime()
+  return function (arg) {
+    let now = new Date().getTime()
+    if (now - oldDate >= delay) {
+      fn(arg.target.value)
+      oldDate = now
+    }
+  }
+}
+
+function throttle1(fn,delay) {
+  let oldDate = new Date().getTime()
+  let _that = this
+  return function () {
+    let now = new Date().getTime()
+    if (now - oldDate >= delay) {
+      fn.apply(_that,args)
+      oldDate = now
+    }
+  }
+}
+
+export {
+  debounce,
+  throttle
+}
---
+++ 英语/npm/npm网站.md
@@ -0,0 +1,6 @@
+* consists -> 构成，组成
+* distinct -> 截然不同的，明显的
+* components -> 组件，部件
+* website -> 网站
+* CLI{ Command Line Interface} -> 命令行
+* registry -> 注册，登记
