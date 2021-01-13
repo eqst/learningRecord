ca4e331f8d02cfd68569a371ae903f93a1df51ec
郑宁 <zn17326432274@63.com>
2021-01-13 14:44:37 +0800

    提交

--- ts/TsStart.ts
+++ ts/TsStart.ts
@@ -13,13 +13,15 @@ write(num)
 interface Person {
     firstName:String;
     lastName:String;
+    middleInitial:String;
+    fullName:String;
 }
 
 function sayName(person: Person) {
     return person.firstName + '' + person.lastName
 }
 
-let user = {firstName:"王",lastName:"宁"}
+let user = {firstName:"王",lastName:"宁",middleInitial:'月',fullName:'王月宁'}
 
 
 document.body.innerHTML = sayName(user);
@@ -34,7 +36,7 @@ class Student {
 let student1 = new Student("王","小","明")
 
 function cslName(student:Person) {
-    return student.firstName+''+student.lastName
+    return student.fullName
 }
 
 document.body.innerHTML = cslName(student1) + '' + sayName(user);
--- ts/tsOutfile/TsStart.js
+++ ts/tsOutfile/TsStart.js
@@ -7,7 +7,7 @@ write(num);
 function sayName(person) {
     return person.firstName + '' + person.lastName;
 }
-var user = { firstName: "王", lastName: "宁" };
+var user = { firstName: "王", lastName: "宁", middleInitial: '月', fullName: '王月宁' };
 document.body.innerHTML = sayName(user);
 var Student = /** @class */ (function () {
     function Student(firstName, middleInitial, lastName) {
@@ -20,6 +20,7 @@ var Student = /** @class */ (function () {
 }());
 var student1 = new Student("王", "小", "明");
 function cslName(student) {
-    return student.firstName + '' + student.lastName;
+    console.log(student1);
+    return student.fullName;
 }
 document.body.innerHTML = cslName(student1) + '' + sayName(user);
